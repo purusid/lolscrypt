c6e291627a7b557c9514749550fb3c7f7b87bfd4
purusidcode <purusid@outlook.com>
2022-12-13 21:22:39 +0530

    lol

--- lolscrypt.js
+++ lolscrypt.js
@@ -9,8 +9,9 @@ const lolScrypt = {
         dbVersion: parseInt(localStorage.getItem("dbVersion")),
         currentProject: "default"
     },
+    editors: class { },
     db: {
-        writeFile: function (data, callback) {
+        writeFile: (data, callback) => {
             lolScrypt.init(function () {
                 if ((data != null) && (lolScrypt.defs.currentProject != null)) {
                     let open = indexedDB.open(lolScrypt.defs.dbName, lolScrypt.defs.dbVersion);
@@ -26,28 +27,32 @@ const lolScrypt = {
                         store.add(data);
                         tx.oncomplete = function () {
                             db.close();
-                            if (callback) callback(true);
+                            if (callback)
+                                callback(true);
                         };
                         tx.onerror = () => {
                             console.error("lolScrypt.db.writeFile: error writing to database: " + tx.error);
                             db.close();
-                            if (callback) callback(false);
+                            if (callback)
+                                callback(false);
                         };
-                    }
+                    };
                     open.onerror = () => {
                         console.error("Error accessing database: " + open.error);
-                        if (callback) callback(false);
-                    }
+                        if (callback)
+                            callback(false);
+                    };
                 }
                 else {
                     console.error("Error: project not defined");
-                    if (callback) callback(false);
+                    if (callback)
+                        callback(false);
                 }
             });
         },
-        readFile: function (path, callback) {
+        readFile: (path, callback) => {
             if (path == "" || path == null) {
-                console.error("lolScrypt.db.readFile: path is empty")
+                console.error("lolScrypt.db.readFile: path is empty");
             }
             else {
                 let open = indexedDB.open(lolScrypt.defs.dbName, lolScrypt.defs.dbVersion);
@@ -57,13 +62,14 @@ const lolScrypt = {
                     let store = tx.objectStore(lolScrypt.defs.currentProject);
                     let req = store.get(path);
                     req.onsuccess = (e) => {
-                        if (callback) callback(req.result.file);
+                        if (callback)
+                            callback(req.result.file);
                         db.close();
                     };
                     req.onerror = function () {
                         console.error(req.error);
                         db.close();
-                    }
+                    };
                     tx.oncomplete = function () {
                         db.close();
                     };
@@ -71,42 +77,44 @@ const lolScrypt = {
                         alert("Error saving file to database: " + tx.error);
                         db.close();
                     };
-                }
+                };
                 open.onerror = () => {
                     alert("Error accessing database: " + open.error);
                     db.close();
-                }
+                };
             }
         },
-        listProjects: function (callback) {
+        listProjects: (callback) => {
             let open = indexedDB.open(lolScrypt.defs.dbName, lolScrypt.defs.dbVersion);
             open.onsuccess = function () {
                 let db = open.result;
                 if (callback) { callback(db.objectStoreNames); }
                 db.close();
-            }
+            };
             open.onerror = () => {
                 alert("Error accessing database: " + open.error);
                 open.close();
-            }
+            };
         },
-        listFiles: function (callback) {
+        listFiles: (callback) => {
             let open = indexedDB.open(lolScrypt.defs.dbName, lolScrypt.defs.dbVersion);
             open.onsuccess = function () {
                 let db = open.result;
                 let tx = db.transaction(lolScrypt.defs.currentProject, "readonly");
                 let store = tx.objectStore(lolScrypt.defs.currentProject);
-                let index = store.index("FileIndex")
+                let index = store.index("FileIndex");
                 let items = [];
                 tx.oncomplete = function () {
                     db.close();
-                    if (callback) callback(items);
+                    if (callback)
+                        callback(items);
                     console.table(items);
                 };
                 tx.onerror = () => {
                     alert("Error saving file to database: " + tx.error);
                     db.close();
-                    if (callback) callback(false);
+                    if (callback)
+                        callback(false);
                 };
                 let cur = index.openCursor();
                 cur.onerror = function (error) {
@@ -119,15 +127,16 @@ const lolScrypt = {
                         cursor.continue();
                     }
                 };
-            }
+            };
             open.onerror = () => {
                 alert("Error accessing database: " + open.error);
                 db.close();
-                if (callback) callback(false);
-            }
+                if (callback)
+                    callback(false);
+            };
         }
     },
-    init: function (callback) {
+    init: (callback) => {
         lolScrypt.db.listProjects((list) => {
             if (Array.from(list).includes(lolScrypt.defs.currentProject)) {
                 proceed();
@@ -145,46 +154,43 @@ const lolScrypt = {
                     let index = store.createIndex("FileIndex", ["file.path", "file.base64data"]);
                 };
                 open.onerror = function () {
-                    if (callback) callback(false)
-                }
-                lolScrypt.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-                    autoCloseTags: true,
-                    autoCloseBrackets: true,
-                    mode: "htmlmixed",
-                    lineNumbers: true,
-                    extraKeys: {
-                        "Ctrl-Space": "autocomplete"
-                    }
-                })
-                lolScrypt.editor.on("keyup", function (cm, event) {
-                    let keyCodeList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "<", "=", " ", "/", "(", ")", "{", "}", "[", "]", "`", "-", "+", "_", "!", "@", "#", "$", "%", "^", "&", "*", ",", ".", ";"]
-                    if (!cm.state.completionActive && keyCodeList.includes(event.key)) {
-                        CodeMirror.commands.autocomplete(cm, null, { completeSingle: false });
-                    }
-                });
+                    if (callback)
+                        callback(false);
+                };
                 document.getElementById("init").style.display = "none";
-                if (callback) { callback(true) }
+                if (callback) { callback(true); }
             }
         });
     },
-    createProject: function () {
+    createProject: () => {
         let n = prompt("Project Name:");
         if (confirm(`Confirm project creation with name '${n}':`)) {
             lolScrypt.defs.currentProject = n;
             lolScrypt.db.writeFile({ file: { path: "/welcome.txt", base64data: "welcome" } });
         } else {
-            return
+            return;
         }
     },
-    setCurrentProject: function (n) {
-        lolScrypt.defs.currentProject = n;
-        lolScrypt.init();
+    reloadFiles: () => {
+        let elem = document.getElementById("nav");
+        elem.innerHTML = null;
+        lolScrypt.db.listFiles((a) => {
+            a.forEach((i) => {
+                elem.innerHTML += `<div onclick="lolScrypt.createEditor('${i.file.path}')">${i.file.path}</div>`
+            })
+        })
     },
-    onload: function () {
+    setCurrentProject: (n) => {
+        lolScrypt.defs.currentProject = n;
+        lolScrypt.init(lolScrypt.reloadFiles());
+    },
+    onload: () => {
         let pls = document.getElementById("pls");
         lolScrypt.db.listProjects((list) => (Array.from(list).forEach((item) => {
-            pls.innerHTML += `<a href="javascript:lolScrypt.setCurrentProject('${item}')"><div>${item}</div><img src="https://purusid.tk/ico/rt-arrow.png" alt="ARROW"></a>`
+            pls.innerHTML += `<a href="javascript:lolScrypt.setCurrentProject('${item}')"><div>${item}</div><img src="https://purusid.tk/ico/rt-arrow.png" alt="ARROW"></a>`;
         })));
     },
-    editor: ''
+    createEditor(n, callback) {
+        lolScrypt.db.readFile(n, (a) => { document.getElementById("code").innerText = a.base64data })
+    }
 }
